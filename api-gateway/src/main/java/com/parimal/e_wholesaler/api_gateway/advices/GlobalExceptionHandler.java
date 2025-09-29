ca39e7063338020e3777b754a80551a5ec53b6ad
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-09-29 22:47:31 +0530

    updated the global exception handler to return the ResponseEntity of ApiResponse instead of just ApiResponse, helpful to detect the JWT expiration commit

--- api-gateway/src/main/java/com/parimal/e_wholesaler/api_gateway/advices/GlobalExceptionHandler.java
+++ api-gateway/src/main/java/com/parimal/e_wholesaler/api_gateway/advices/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.parimal.e_wholesaler.api_gateway.advices;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,27 +15,27 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse handleException(Exception e) {
+    public ResponseEntity<ApiResponse> handleException(Exception e) {
         return buildApiErrorResponse(e.getMessage(), Collections.emptyList(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public ApiResponse handleExpiredJwtException(ExpiredJwtException e) {
+    public ResponseEntity<ApiResponse> handleExpiredJwtException(ExpiredJwtException e) {
         return buildApiErrorResponse("JWT Expired", Collections.emptyList(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(JwtException.class)
-    public ApiResponse handleJwtException(JwtException e) {
+    public ResponseEntity<ApiResponse> handleJwtException(JwtException e) {
         return buildApiErrorResponse(e.getMessage(), Collections.emptyList(), HttpStatus.UNAUTHORIZED);
     }
 
-    private ApiResponse buildApiErrorResponse(String message, List<String> subErrors, HttpStatus status) {
+    private ResponseEntity<ApiResponse> buildApiErrorResponse(String message, List<String> subErrors, HttpStatus status) {
         ApiError apiError = ApiError.builder()
                 .message(message)
                 .subErrors(subErrors)
                 .status(status)
                 .build();
-        return new ApiResponse(apiError);
+        return new ResponseEntity(new ApiResponse(apiError), status);
     }
 
 }
